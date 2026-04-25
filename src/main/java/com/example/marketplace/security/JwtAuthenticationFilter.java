f06f59cfca862676d3755360d5d1ef4cc58707d7
prikkoldess <sashapatenko13@gmail.com>
2026-04-25 12:56:50 +0300

    security

--- src/main/java/com/example/marketplace/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/marketplace/security/JwtAuthenticationFilter.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
