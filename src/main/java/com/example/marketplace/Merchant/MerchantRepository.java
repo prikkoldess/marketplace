6efdcf5c1ce403af026bdba111aa3aa85462bd25
prikkoldess <sashapatenko13@gmail.com>
2026-08-22 18:34:12 +0300

    refactor: clean up old package paths

--- src/main/java/com/example/marketplace/Merchant/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.marketplace.Merchant;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MerchantRepository extends JpaRepository<Merchant, Long> {
-    Optional<Merchant> findByInviteCode(String inviteCode);
-}
