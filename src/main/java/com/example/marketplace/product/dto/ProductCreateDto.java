4220165b74e72fc2cac13691fb563674eeddd452
prikkoldess <sashapatenko13@gmail.com>
2026-08-22 18:05:33 +0300

    feat: add validation

--- src/main/java/com/example/marketplace/product/dto/ProductCreateDto.java
+++ src/main/java/com/example/marketplace/product/dto/ProductCreateDto.java
@@ -2,9 +2,16 @@ package com.example.marketplace.product.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record ProductCreateDto(
-        String title,
-        Integer quantity,
-        BigDecimal price) {
+                @NotBlank(message = "Title cannot be empty") String title,
+
+                @NotNull(message = "Quantity is required") @Min(value = 0, message = "Quantity cannot be negative") Integer quantity,
+
+                @NotNull(message = "Price is required") @Positive(message = "Price must be greater than zero") BigDecimal price) {
 
 }
--- src/main/java/com/example/marketplace/product/dto/ProductUpdateDto.java
+++ src/main/java/com/example/marketplace/product/dto/ProductUpdateDto.java
@@ -3,10 +3,12 @@ package com.example.marketplace.product.dto;
 import java.math.BigDecimal;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 
 public record ProductUpdateDto(
-                @Schema(example = "10") Integer quantity,
-                BigDecimal price)
+        @Min(value = 0, message = "The quantity cannot be negative.") @Schema(example = "10") Integer quantity,
+        @Positive(message = "Price must be greater than zero") BigDecimal price)
 
 {
 }
